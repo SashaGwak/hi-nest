557f6570a644ad2755fd8edb4d44e497a1bacdf3
sihagwak <sasha.gwak@gmail.com>
2022-11-07 21:52:28 +0900

    feat: hello route 생성해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,15 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // string을 리턴하는 getHello라는 함수
   getHello(): string {
+    // string을 리턴하는 getHello라는 함수
     return this.appService.getHello();
+  }
+
+  // 데코레이터는 꾸며주는 함수나 클래스랑 붙어있어야함. 공백X
+  // app.get('/hello')랑 같은 의미
+  @Get('/hello')
+  sayHello(): string {
+    return 'Hello everyone';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 // 데코레이터는 클래스에 함수기능을 추가할 수 있음(걍 클래스 위의 함수)
 @Module({
   imports: [],
+  // controllers는 express의 라우터 같은 존재(url가져오고 함수 실행)[연결된 컨트롤러]
   controllers: [AppController],
   providers: [AppService],
 })
